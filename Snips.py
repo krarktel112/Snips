0abaaa6204da0f0ad2c50fca8cfd95b72fecf691
krarktel112 <krarktel@gmail.com>
2025-04-12 11:19:28 -0400

    Update Snips.py

--- Snips.py
+++ Snips.py
@@ -315,3 +315,14 @@ print(ST.scenes)
 # format of capabilities, commands, and arguments
 
 ST.execute_scene(ST.scenes['Home']['Ashley4'])
+
+import SmartThings
+
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+print("scenes")
+print(ST.scenes)
+print("devices")
+print(ST.devices)
+print("locations")
+print(ST.locations)
