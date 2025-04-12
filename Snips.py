2ce90384fc60764fa0533a05563bf0f4d573f3d2
krarktel112 <krarktel@gmail.com>
2025-04-12 11:18:16 -0400

    Update Snips.py

--- Snips.py
+++ Snips.py
@@ -278,3 +278,40 @@ async function executeRoutine() {
 }
 
 executeRoutine();
+
+import SmartThings
+
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+print(ST.scenes)
+# Reference the SmartThings API documentation for information regarding the
+# format of capabilities, commands, and arguments
+
+ST.execute_scene(ST.scenes['Home']['Ashley1'])
+import SmartThings
+
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+print(ST.scenes)
+# Reference the SmartThings API documentation for information regarding the
+# format of capabilities, commands, and arguments
+
+ST.execute_scene(ST.scenes['Home']['Ashley2'])
+import SmartThings
+
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+print(ST.scenes)
+# Reference the SmartThings API documentation for information regarding the
+# format of capabilities, commands, and arguments
+
+ST.execute_scene(ST.scenes['Home']['Ashley1'])
+import SmartThings
+
+token = '372563b7-09f8-485b-8c95-261793424ad9'
+ST = SmartThings.Account(token)
+print(ST.scenes)
+# Reference the SmartThings API documentation for information regarding the
+# format of capabilities, commands, and arguments
+
+ST.execute_scene(ST.scenes['Home']['Ashley4'])
